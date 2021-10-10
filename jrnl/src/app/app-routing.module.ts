288405628758882a982889f9c7eb1939fcebff25
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-10 07:57:40 -0300

    Autenticação com firebase

--- jrnl/src/app/app-routing.module.ts
+++ jrnl/src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AcessoComponent } from './acesso/acesso.component';
 import { Error404Component } from './error404/error404.component';
 import { GestaoFuncionalidadesComponent } from './gestao-funcionalidades/gestao-funcionalidades.component';
 import { HomeComponent } from './home/home.component';
@@ -8,9 +9,11 @@ import { AutenticacaoGuard } from './services/autenticacao-guard.service'
 export const ROUTES: Routes = [
   
   { path: '', component: MainComponent} ,
+  { path: 'acesso', component: AcessoComponent },
   { path: 'gestao', component: GestaoFuncionalidadesComponent, canActivate: [AutenticacaoGuard] },
   { path: '**', component: Error404Component },
-   {path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent }
+  
   
  ];
  
--- jrnl/src/app/app.module.ts
+++ jrnl/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -11,11 +13,12 @@ import { ROUTES } from './app-routing.module';
 import { Error404Component } from './error404/error404.component';
 import { MainComponent } from './main/main.component';
 import { GestaoFuncionalidadesComponent } from './gestao-funcionalidades/gestao-funcionalidades.component';
-import { AcessoComponent } from './acesso/acesso.component';
+
 import { LoginComponent } from './acesso/login/login.component';
 import { Autenticacao } from './services/autenticacao.service';
 import { AutenticacaoGuard } from './services/autenticacao-guard.service';
 import { Bd } from "./services/bd.service"
+import { AcessoComponent } from './acesso/acesso.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +29,13 @@ import { Bd } from "./services/bd.service"
     Error404Component,
     MainComponent,
     GestaoFuncionalidadesComponent,
+    LoginComponent,
     AcessoComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [Autenticacao, AutenticacaoGuard, Bd],
