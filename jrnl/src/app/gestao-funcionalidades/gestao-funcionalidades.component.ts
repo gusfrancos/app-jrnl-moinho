9f67811a98528325e9279e1c1f93237d612b5faf
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-20 20:57:02 -0300

    Noticias

--- jrnl/src/app/gestao-funcionalidades/gestao-funcionalidades.component.ts
+++ jrnl/src/app/gestao-funcionalidades/gestao-funcionalidades.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Autenticacao } from '../services/autenticacao.service';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-gestao-funcionalidades',
@@ -9,7 +10,9 @@ import { Autenticacao } from '../services/autenticacao.service';
 })
 export class GestaoFuncionalidadesComponent implements OnInit {
 
-  constructor(private autenticacao: Autenticacao) { }
+  constructor(private autenticacao: Autenticacao, private router: Router) { }
+
+  public verNoticia: boolean = false;
 
   ngOnInit(): void {
   }
@@ -17,6 +20,12 @@ export class GestaoFuncionalidadesComponent implements OnInit {
   public sair() : void {
     console.log('SAIR')
     this.autenticacao.sair();
+    this.router.navigate(['acesso'])
+  }
+
+  public irPara(menu: string) : void {
+    console.log('VER_NOTICIA: TRUE' )
+    this.verNoticia = true;
   }
 
 }
