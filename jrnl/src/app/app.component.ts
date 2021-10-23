0c336a63756814db5abdcf1faecab13620d777bb
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-23 19:19:10 -0300

    Main

--- jrnl/src/app/app.component.ts
+++ jrnl/src/app/app.component.ts
@@ -17,13 +17,3 @@ export class AppComponent {
   }
 }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAN46KBKqig_4OaZNuBe42SNu_fy1O35Fo",
-  authDomain: "rep-app-moinho.firebaseapp.com",
-  databaseURL: "https://rep-app-moinho-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rep-app-moinho",
-  storageBucket: "rep-app-moinho.appspot.com",
-  messagingSenderId: "506340719362",
-  appId: "1:506340719362:web:84aaaec3a66cd934d5e01b",
-  measurementId: "G-RNZ009R08V"
-};
