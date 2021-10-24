4618a430f19545be383a29de138418f0181784df
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-24 19:56:05 -0300

    ServicoNoticias

--- jrnl/src/app/services/noticias.service.ts
+++ jrnl/src/app/services/noticias.service.ts
@@ -1,14 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
+
+import { Noticia } from '../models/noticia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiasService {
 
-  constructor() { }
+  private readonly API = '../../assets/noticias/noticias.json'
+  constructor(private httpClient: HttpClient) { }
 
-  list() {
-    return {_id: '1', categoria: 'Hydrogen', titulo: 'Com Dona Flor', descricao_noticia: 'descricao_noticia'};
+  listAll() {
+    return this.httpClient.get<Noticia[]>(this.API)
+    .pipe(
+      first(),
+      tap(not => console.log(not)))
   }
 
 
