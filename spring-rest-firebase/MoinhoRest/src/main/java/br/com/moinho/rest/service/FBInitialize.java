91a44e54cd9acd9dcaf0b2ec6e649c6ea9a8de14
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-25 06:12:02 -0300

    InitSpring

--- spring-rest-firebase/MoinhoRest/src/main/java/br/com/moinho/rest/service/FBInitialize.java
+++ spring-rest-firebase/MoinhoRest/src/main/java/br/com/moinho/rest/service/FBInitialize.java
@@ -4,11 +4,17 @@ import java.io.FileInputStream;
 
 import javax.annotation.PostConstruct;
 
+
 import org.springframework.stereotype.Service;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.auth.oauth2.GoogleCredentials;
+
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 
 @Service
 public class FBInitialize {
@@ -16,15 +22,22 @@ public class FBInitialize {
     @PostConstruct
     public void initialize() {
         try {
+        	 log.info(this.getClass().getResource(".").getPath() );
             FileInputStream serviceAccount =
-                    new FileInputStream("./rep-app-moinho-firebase-admin.json");
-
+                    new FileInputStream(this.getClass().getResource(".").getPath() + "rep-app-moinho-firebase-admin.json");
+            
+            log.info("FBInitialize: Abriu o Json com chave de acesso." );
+            
+            
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://rep-app-moinho-default-rtdb.asia-southeast1.firebasedatabase.app")
                     .build();
+            log.info("FBInitialize: Abriu opções de inicialização");
 
             FirebaseApp.initializeApp(options);
+            log.info("FBInitialize: Inicializou");
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
