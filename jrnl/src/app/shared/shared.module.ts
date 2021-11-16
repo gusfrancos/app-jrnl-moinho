2b07764ce6f7f16aa2d97e2959ab3b314ccaf47a
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-16 07:21:30 -0300

    Pipe

--- jrnl/src/app/shared/shared.module.ts
+++ jrnl/src/app/shared/shared.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CategoryPipe } from './pipes/category.pipe';
 
 
 @NgModule({
-  declarations: [ ],
+  declarations: [
+    CategoryPipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CategoryPipe
   ]
 })
 export class SharedModule { }
